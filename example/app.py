d0a706dc52df0f0aefaab0b2a2d7dcd41fe35c98
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-07-06 20:40:09 +0900

    Adding example code.

---
+++ example/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
